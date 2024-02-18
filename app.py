fd085c4911f43b60f29676aeb3abed1b3d83ce5b
Rafael Takeguma <rafael08.0807@gmail.com>
2024-02-18 00:10:54 -0300

    implementa logica inicial do arquivo usuarios.json

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ import json
 
 # Cria um objeto de aplicação Flask
 app = Flask(__name__)
+
+# Caminho para o arquivo que armazena os dados dos usuários
+USUARIOS = 'usuarios.json'
+
+# Checa se o arquivo que armazena os dados existe, se não existir, cria um
+try:
+    with open(USUARIOS, 'r') as file:
+        pass
+except FileNotFoundError:
+    with open(USUARIOS, 'w') as file:
+        json.dump([], file)

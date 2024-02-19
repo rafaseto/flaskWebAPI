89c612db502600ff39962de4bc74e646f33af18c
Rafael Takeguma <rafael08.0807@gmail.com>
2024-02-19 11:20:18 -0300

    adiciona arquivo linkVideo

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-import requests
-
-# Requisição POST
-dados_requisicao = {
-    "cpf": 12345678921,
-    "nome": "João Guilherme",
-    "data_nascimento": "1990-02-01"
-}
-
-my_post = requests.post('http://127.0.0.1:5000/usuario', json=dados_requisicao)
-print(my_post.json())
+app.run()
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import requests
 
 # Requisição POST
 dados_requisicao = {
-    "cpf": 12345672222,
-    "nome": "Joane H",
-    "data_nascimento": "1980-12-01"
+    "cpf": 99999999999,
+    "nome": "Gabriel Silva",
+    "data_nascimento": "2000-05-01"
 }
 
-my_post = requests.post('http://127.0.0.1:5000/usuario', json=dados_requisicao)
-print(my_post.json())
+#my_post = requests.post('http://127.0.0.1:5000/usuario', json=dados_requisicao)
+#print(my_post.json())
 
-#my_get = requests.get('http://127.0.0.1:5000/usuario/12345678900')
-#print(my_get.json())
+my_get = requests.get('http://127.0.0.1:5000/usuario/12345678900')
+print(my_get.json())
